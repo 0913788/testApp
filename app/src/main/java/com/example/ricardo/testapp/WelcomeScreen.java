41d93c5ff94f59d8af40ef753b869a2459ca928e
Ricardo Stam <Ricardo.stam@protonmail.ch>
2018-02-07 23:33:51 +0100

    updated as shown

--- app/src/main/java/com/example/ricardo/testapp/WelcomeScreen.java
+++ app/src/main/java/com/example/ricardo/testapp/WelcomeScreen.java
@@ -9,5 +9,6 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        // updated
     }
 }
